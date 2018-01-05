62ed6145820a87449d1b2638129e86226168f320
maitri <maitrivelani999@gmail.com>
2018-01-05 17:05:29 +0530

    method added to main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Display display = new Display();
         display.show();
+
+        FirstBranchClass fbc = new FirstBranchClass();
+        fbc.firstMessage();
+        fbc.secondMessage();
     }
 }
